f0c49d03b40b220889ac958c959f730f5558b7ad
jasonbgilroy <110847892+jasonbgilroy@users.noreply.github.com>
2023-12-11 12:11:55 -0500

    load balancer

--- main.tf
+++ main.tf
@@ -63,6 +63,10 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = aws_instance.blog.id
+        port = 80
+      }
     }
   ]
 
